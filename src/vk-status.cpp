64c73b67268eb1e2485070b7b679c8392f190e2c
fleissig <narc@bk.ru>
2014-01-25 01:33:35 +0400

    invisible status

--- src/vk-status.cpp
+++ src/vk-status.cpp
@@ -9,14 +9,18 @@ void vk_update_status(PurpleConnection* gc)
 {
     PurpleStatus* status = purple_account_get_active_status(purple_connection_get_account(gc));
     PurpleStatusPrimitive primitive_status = purple_status_type_get_primitive(purple_status_get_type(status));
-    if (primitive_status != PURPLE_STATUS_AVAILABLE) {
-        purple_debug_info("prpl-vkcom", "Status is not Available, setting offline\n");
+    if (primitive_status == PURPLE_STATUS_OFFLINE) {
+        purple_debug_info("prpl-vkcom", "Status is Offline, setting offline\n");
         vk_set_offline(gc);
-        return;
     }
-
-    purple_debug_info("prpl-vkcom", "Status is Available, setting online\n");
-    vk_call_api(gc, "account.setOnline", {});
+    else if(primitive_status == PURPLE_STATUS_AVAILABLE) {
+        purple_debug_info("prpl-vkcom", "Status is Available, setting online\n");
+        vk_call_api(gc, "account.setOnline", {});
+    }
+    else if(primitive_status == PURPLE_STATUS_INVISIBLE) {
+        purple_debug_info("prpl-vkcom", "Status is Invisible, setting offline\n");
+        vk_set_offline(gc);
+    }
 }
 
 void vk_set_offline(PurpleConnection* gc)
