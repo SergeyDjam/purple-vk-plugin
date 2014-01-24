48026c8231aee1a4f36aecc8ab3b671738343a15
Oleg Andreev <OlegOAndreev@yandex.ru>
2014-01-25 03:09:09 +0400

    Fixed changing to away not setting offline status in Vk.com

--- src/vk-status.cpp
+++ src/vk-status.cpp
@@ -9,18 +9,31 @@ void vk_update_status(PurpleConnection* gc)
 {
     PurpleStatus* status = purple_account_get_active_status(purple_connection_get_account(gc));
     PurpleStatusPrimitive primitive_status = purple_status_type_get_primitive(purple_status_get_type(status));
-    if (primitive_status == PURPLE_STATUS_OFFLINE) {
-        purple_debug_info("prpl-vkcom", "Status is Offline, setting offline\n");
+    switch (primitive_status) {
+    case PURPLE_STATUS_AVAILABLE:
+        purple_debug_info("prpl-vkcom", "Status is Available, setting online\n");
+        vk_set_online(gc);
+        break;
+    case PURPLE_STATUS_AWAY:
+        purple_debug_info("prpl-vkcom", "Status is Away, setting offline\n");
         vk_set_offline(gc);
-    }
-    else if(primitive_status == PURPLE_STATUS_INVISIBLE) {
+        break;
+    case PURPLE_STATUS_INVISIBLE:
         purple_debug_info("prpl-vkcom", "Status is Invisible, setting offline\n");
         vk_set_offline(gc);
+        break;
+    case PURPLE_STATUS_OFFLINE:
+        purple_debug_info("prpl-vkcom", "Status is Offline, setting offline\n");
+        vk_set_offline(gc);
+        break;
+    default:
+        assert(false);
     }
-    else {
-        purple_debug_info("prpl-vkcom", "Status is Available, setting online\n");
-        vk_call_api(gc, "account.setOnline", {});
-    }
+}
+
+void vk_set_online(PurpleConnection* gc)
+{
+    vk_call_api(gc, "account.setOnline", {});
 }
 
 void vk_set_offline(PurpleConnection* gc)
