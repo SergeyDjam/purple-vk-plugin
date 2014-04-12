8e065a8f1ec5168e08113c90ce18e061862c0e2a
Oleg Andreev <OlegOAndreev@yandex.ru>
2014-04-12 12:50:13 +0400

    Additional checks and debug output for binary installation script.

--- dist/bin/install.sh
+++ dist/bin/install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ $EUID != 0 ]; then
+  echo "Please run this script as root, e.g. sudo $0"
+  exit 1
+fi
+
 if [ `uname -m` == x86_64 ]; then
   if [ -d /usr/lib64/purple-2 ]; then
+    echo "Copying plugin to /usr/lib64/purple-2"
     cp bin/x86_64/*.so /usr/lib64/purple-2
   else
     if [ -d /usr/lib/purple-2 ]; then
+      echo "Copying plugin to /usr/lib/purple-2"
       cp bin/x86_64/*.so /usr/lib/purple-2
     else
       echo "ERROR: Either pidgin is not installed or is installed in unknown location"
@@ -15,6 +22,7 @@ else
     echo "ERROR: Either pidgin is not installed or is installed in unknown location"
     exit 1
   fi
+  echo "Copying plugin to /usr/lib/purple-2"
   cp bin/i386/*.so /usr/lib/purple-2
 fi
 
